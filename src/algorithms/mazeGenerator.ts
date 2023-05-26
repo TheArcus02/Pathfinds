7075183ce4780f076d73e2d2478ddd1f6e148749
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-05-26 21:57:18 +0200

    continue work on prim algorithm

--- src/algorithms/mazeGenerator.ts
+++ src/algorithms/mazeGenerator.ts
@@ -15,17 +15,59 @@ const getWalls = (nodes: INode[][], node: INode): INode[] => {
   return walls.filter((wall) => wall.isWall);
 };
 
+const getNeighboringNode = (nodes: INode[][], node: INode): INode | null => {
+  const { row, col } = node;
+
+  const topNode = row > 0 ? nodes[row - 1][col] : null;
+  const bottomNode = row < nodes.length - 1 ? nodes[row + 1][col] : null;
+  const leftNode = col > 0 ? nodes[row][col - 1] : null;
+  const rightNode = col < nodes[0].length - 1 ? nodes[row][col + 1] : null;
+
+  if (topNode && !topNode.isVisited) return topNode;
+  if (bottomNode && !bottomNode.isVisited) return bottomNode;
+  if (leftNode && !leftNode.isVisited) return leftNode;
+  if (rightNode && !rightNode.isVisited) return rightNode;
+
+  return null;
+};
+
 const prim = (nodes: INode[][]) => {
   const startingNode = nodes[0][0];
   startingNode.isWall = false;
+  startingNode.isVisited = true;
+
   const walls = getWalls(nodes, startingNode);
+
   while (walls.length > 0) {
     const randomWall = walls[getRandomInteger(0, walls.length - 1)];
+    const neighboringNode = getNeighboringNode(nodes, randomWall);
 
-    randomWall.isWall = false;
-    const newWalls = getWalls(nodes, randomWall);
-    walls.push(...newWalls);
+    if (neighboringNode && neighboringNode.isWall) {
+      randomWall.isWall = false;
+
+      if (neighboringNode.row === randomWall.row) {
+        const colIndex =
+          randomWall.col < neighboringNode.col
+            ? randomWall.col + 1
+            : randomWall.col - 1;
+
+        nodes[randomWall.row][colIndex].isWall = false;
+      } else {
+        const rowIndex =
+          randomWall.row < neighboringNode.row
+            ? randomWall.row + 1
+            : randomWall.row - 1;
+
+        nodes[rowIndex][randomWall.col].isWall = false;
+      }
+
+      const newWalls = getWalls(nodes, randomWall);
+      walls.push(...newWalls);
+      neighboringNode.isVisited = true;
+      randomWall.isVisited = true;
+    }
     walls.splice(walls.indexOf(randomWall), 1);
+
     // * Debugging
     // const nodesWithWalls = nodes
     //   .flatMap((node) => node)
